577665385d2df3398c808f9215dfe0c98a78016a
Rishik0w0 <brishik2003@gmail.com>
2024-05-09 13:56:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import { getCount,increment,decrement } from "./count.js"
+
+console.log(getCount())
+increment()
+console.log(getCount())
+increment()
+console.log(getCount())
+decrement()
+console.log(getCount())
---
+++ count.js
@@ -0,0 +1,10 @@
+
+let count = 0
+
+const getCount  = ()=>count
+
+const increment = ()=>{count++}
+
+const decrement = ()=>{count--}
+
+export {getCount,increment,decrement}
